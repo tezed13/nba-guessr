7dfdb0d6e7559095182f581ea605488bc2440fa4
tezed13 <jpcortez130@gmail.com>
2026-01-25 15:46:07 -0500

    Revert "da"
    This reverts commit df68a1f4a01e15330efe4ebed0272d4499276515.

--- static/js/game.js
+++ static/js/game.js
@@ -50,16 +50,7 @@ async function loadRandomPlayer() {
 
   if (data.error) {
     playerNameEl.textContent = "Error";
-    seasonsBody.innerHTML = `<tr><td colspan="8" class="text-center text-red-600 p-2">${data.error}</td></tr>`;row.innerHTML = `
-  <td class="border border-gray-700 p-2">${season.season}</td>
-  <td class="border border-gray-700 p-2">${season.tm}</td>
-  <td class="border border-gray-700 p-2">${season.g}</td>
-  <td class="border border-gray-700 p-2">${season.gs}</td>
-  <td class="border border-gray-700 p-2">${season.mp_per_game}</td>
-  <td class="border border-gray-700 p-2">${season.pts_per_game}</td>
-  <td class="border border-gray-700 p-2">${season.ast_per_game}</td>
-  <td class="border border-gray-700 p-2">${season.trb_per_game}</td>
-`;
+    seasonsBody.innerHTML = `<tr><td colspan="8" class="text-center text-red-600 p-2">${data.error}</td></tr>`;
     return;
   }
 
